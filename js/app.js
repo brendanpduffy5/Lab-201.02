877e3037972d86f9412deae1858bf45b31ca25f2
Brendan Duffy <121203064+brendanpduffy5@users.noreply.github.com>
2023-02-18 21:22:03 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,139 +1,109 @@
 'use strict';   
-
 let userName = prompt("Hello! What is your name?");
+let score = 0;
+let answer1 = prompt("Welcome to my site, " + userName + "!  Do I live in Colorado?").toLowerCase();
 
 function question1(){
-    let answer1 = prompt("Welcome to my site, " + userName + "!  Do I live in Colorado?").toLowerCase();
-
-if (answer1 == "yes")
-//    console.log("Correct!");
+if (answer1 == "yes" || answer1 == "y") {
     alert("Correct!");
-else if (answer1 == "y")
-//    console.log("Correct!");
-    alert("Correct!");
-else if (answer1 == "no")
-//    console.log("That is incorrect.");
-    alert("That is incorrect.");
-else if (answer1 == "n")
-//    console.log("That is incorrect.");
-    alert("That is incorrect.");
-else {
-//    console.log("Please reply Yes or No.")
+    score++;
+} else if (answer1 == "no" || answer1 == "n" ) {
+alert("That is incorrect.");
+} else {
     alert("Please reply Yes or No.");
 }
 }
 question1();
 
 function question2(){
-    let answer2 = prompt("Did I attend college in Maine?").toLowerCase();
-if (answer2 == "no")
-//    console.log("Correct!");
+let answer2 = prompt("Did I attend college in Maine?").toLowerCase();
+if (answer2 == "no" || answer2 == "n" ) {
     alert("Correct!");
-else if (answer2 == "n")
-//    console.log("Correct!");
-    alert("Correct!");
-else if (answer2 == "yes")
-//    console.log("That is incorrect.");
-    alert("That is incorrect.");
-else if (answer2 == "y")
-//    console.log("That is incorrect.");
-    alert("That is incorrect.");
-else {
-//    console.log("Please reply Yes or No.")
+    score++;
+}else if (answer2 == "yes" || answer2 == "y"){
+alert("That is incorrect.");
+}else {
     alert("Please reply Yes or No.");
 }
 }
 question2();
 
-
 function question3(){
-    let answer3 = prompt("Am I originally from New Hampshire?").toLowerCase();
-    if (answer3 == "no")
-    //    console.log("Correct!");
-        alert("Correct!");
-    else if (answer3 == "n")
-    //    console.log("Correct!");
-        alert("Correct!");
-    else if (answer3 == "yes")
-    //    console.log("That is incorrect.");
-        alert("That is incorrect.");
-    else if (answer3 == "y")
-    //    console.log("That is incorrect.");
-        alert("That is incorrect.");
-    else {
-    //    console.log("Please reply Yes or No.")
-        alert("Please reply Yes or No.");
+let answer3 = prompt("Am I originally from New Hampshire?").toLowerCase();
+if (answer3 == "no" || answer3 == "n" ) {
+    alert("Correct!");
+    score++;
+}else if (answer3 == "yes" || answer3 == "y"){
+alert("That is incorrect.");
+}else {
+    alert("Please reply Yes or No.");
 }
 }
 question3();
 
-
-function question4 (){
-    let answer4 = prompt("Do I have four kids?").toLowerCase();
-    if (answer4 == "yes")
-    //    console.log("Correct!");
+function question4(){
+let answer4 = prompt("Do I have four kids?").toLowerCase();
+if (answer4 == "yes" || answer4 == "y") {
         alert("Correct!");
-    else if (answer4 == "y")
-    //    console.log("Correct!");
-        alert("Correct!");
-    else if (answer4 == "no")
-    //    console.log("That is incorrect.");
-        alert("That is incorrect.");
-    else if (answer4 == "n")
-    //    console.log("That is incorrect.");
-        alert("That is incorrect.");
-    else {
-    //    console.log("Please reply Yes or No.")
+        score++;
+    } else if (answer4 == "no" || answer4 == "n") {
+    alert("That is incorrect.");
+    } else {
         alert("Please reply Yes or No.");
     }
 }
-question4 ();
+question4();
 
+function question5(){
+let answer5 = prompt("Is my main goal to become a full stack software developer? ").toLowerCase();
+if (answer5 == "yes" || answer5 == "y") {
+        alert("Correct!");
+        score++;
+    } else if (answer5 == "no" || answer5 =="n" ) {
+    alert("That is incorrect.");
+    } else {
+        alert("Please reply Yes or No.");
+    }
+}
+question5();
 
-// let answer5 = prompt("Is my main goal to become a full stack software developer? ").toLowerCase();
-// if (answer5 == "yes")
-// //    console.log("Correct!");
-//     alert("Correct!");
-// else if (answer5 == "y")
-// //    console.log("Correct!");
-//     alert("Correct!");
-// else if (answer5 == "no")
-// //    console.log("That is incorrect.");
-//     alert("That is incorrect.");
-// else if (answer5 == "n")
-// //    console.log("That is incorrect.");
-//     alert("That is incorrect.");
-// else {
-// //    console.log("Please reply Yes or No.")
-//     alert("Please reply Yes or No.");
-// }
+function question6(){
+    for(let i = 0; i < 4; i++) {
 
+        let answer6 = prompt("How many dogs do you think I have?"); 
+            if(answer6 > 2){
+            alert("That's too high!  Try again...");
+        } else if(answer6 < 2) {
+            alert("That is too low.  Try again!");
+        } else{
+            alert("Yes! I have two dogs!")
+            score++;
+            break;
+        }
+    }
+}
+question6();
 
+let userAttempts = 6;
 
-// for(let i = 0; i < 4; i++) {
+let myDogs = ['german shepherd dog', 'siberian husky', 'husky'];
+function question7(){
+    while(userAttempts > 0) {
 
-//     let answer6 = prompt("How many dogs do you think I have?"); 
-//         if(answer6 > 2){
-//         alert("That's too high!  Try again...");
-//     } else if(answer6 < 2) {
-//         alert("That is too low.  Try again!");
-//     } else{
-//         alert("Yes! I have two dogs!")
-//         break;
-//     }
-// }
-
-// for(let i=1; i < 6; i++); {
-//     let answer7 = prompt("What type of dogs do I have?").toLowerCase();
+        let answer7 = prompt('What type of dogs do I have?').toLowerCase();
+        userAttempts--;
     
-//     if (answer7 == 'german shepherd dog' || 'german shepherd'){
-//         alert ('Yes! I have one German Shepherd Dog named Charlie. He is so smart!');
-//     } else if (answer7 == "siberian husky" || 'husky') {
-//         alert('Yes! I have a Siberian Husky named Mia. She gets in a lot of trouble!');
-//     } else {
-//         alert("No.  Cool dog though.  Try again?");
-//     }
-// }
+        for(let j = 0; j < myDogs.length; j++){
+            if (myDogs[j] === answer7) {
+                alert('Yes! That\'s right! I have a German Shepherd Dog and a Siberian Husky.');
+                score++;
+                break;
+            }
+        }
+    } alert('Sorry you are out of guesses.  I have a German Shepherd Dog and a Siberian Husky.');
+}
+question7();
 
-// alert("Thanks for stopping by, " + userName + "!  Please visit again!");
+alert("Thanks for playing! You got " + score + " out of 7 questions correct.");
 
+alert("Goodbye, " + userName + "!  Please visit again!");
